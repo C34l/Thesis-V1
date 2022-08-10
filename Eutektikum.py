13e907751e9015f2c12bd36e601df1658c230f41
C34l <u.garbe95@gmail.com>
2022-08-10 15:17:10 +0200

    10.08 modifizierter SLE-Ansatz 2

--- Eutektikum.py
+++ Eutektikum.py
@@ -53,6 +53,17 @@ class EutFind:
         return _func
 
     @staticmethod
+    def t_sle_modifiziert_nrtl_beidseitig(_texp, _xi, _h0a, _t0a, _ga, _h0b, _t0b, _gb, _alpha):
+
+        _ya = EutFind.y_nrtl(_ga, _xi, _texp, _alpha)
+        _yb = EutFind.y_nrtl(_gb, 1-_xi, _texp, _alpha)
+        _ha = ((_h0a / (_r * _texp)) * (1 - (_texp / _t0a)))
+        _hb = ((_h0b / (_r * _texp)) * (1 - (_texp / _t0b)))
+        _gx = 16 * _xi * h.np.exp(_ya) * (1-_xi) * h.np.exp(_yb)
+        _func = (h.np.exp(0.5*(_ha+_hb))/_gx)-1
+        return _func
+
+    @staticmethod
     def t_sle_modifiziert_porter(_texp, _xi, _h0, _t0, _a1, _a2):
 
         _yi = EutFind.y_porter(_xi, _texp, _a1, _a2)
@@ -859,6 +870,104 @@ class EutFind:
 
                 return 0
 
+            def _nrtl_pure_comp_SLE_eigen_beidseitig():
+                _xin = h.np.zeros(100)
+                _xinRac = h.np.zeros(100)
+                _xinRac[0] = 0.25
+                _tin = h.np.zeros(100)
+                _tin[0] = 273.15
+
+                _tcalcS = h.np.zeros(100)
+                _tcalcRSS = h.np.zeros(100)
+                _tcalcRSR = h.np.zeros(100)
+                _tcalcR = h.np.zeros(100)
+
+
+                # loading up values
+                for x in range(len(_xin)):
+                    _xin[x] = _xin[x - 1] + 0.01
+                    _xinRac[x] = _xinRac[0]+.003*x
+
+                for x in range(len(_tin)):
+                    _tin[x] = _tin[0] + (2.0 * x)
+
+                # loading up calc points
+                for x in range(len(_xin)):
+                    _tcalcS[x] = h.spo.fsolve(EutFind.t_sle, _tin[x], args=(_xin[x], _h0S, _t0S, _gSa, _Alpha))
+                    _tcalcRSS[x] = h.spo.fsolve(EutFind.t_sle_modifiziert_nrtl_beidseitig, _tin[x], args=(_xin[x], _h0S, _t0S, _gSa, _h0R, _t0R, _gRa, _Alpha))
+                    _tcalcRSR[x] = h.spo.fsolve(EutFind.t_sle_modifiziert_nrtl_beidseitig, _tin[x], args=(_xin[x], _h0S, _t0S, _gSa, _h0R, _t0R, _gRa, _Alpha))
+                    _tcalcR[x] = h.spo.fsolve(EutFind.t_sle, _tin[x], args=(_xin[x], _h0R, _t0R, _gRa, _Alpha))
+
+                # plotting
+                figure, axis = plt.subplots(3, constrained_layout=True)
+
+
+                axis[0].plot(1-_xin, _tcalcS, '-g', label='S-Ma-NRTL')
+                axis[0].plot(1-_xin, _tcalcRSS, '--g', label='Rac-Ma-SLE_eigen')
+                axis[0].set_title("S-Rac-Porter_GD")
+                axis[0].set_ylabel('Temperatur [K]')
+                axis[0].set_xlabel('x-R-Ma [-]')
+                axis[0].legend()
+
+                line_1 = LineString(h.np.column_stack((1-_xin, _tcalcS)))
+                line_2 = LineString(h.np.column_stack((_xinRac, _tcalcRSS)))
+                intersection = line_1.intersection(line_2)
+
+                #axis[0].plot(*intersection.xy, 'ro')
+                #x, y = intersection.xy
+                #xout0 = x[0]
+                #xoutstr0 = "{:10.4f}".format(xout0)
+                #yout0 = y[0]
+                #youtstr0 = "{:10.4f}".format(yout0)
+                #stringout0 = str(f"SP({xoutstr0}"f"\\{youtstr0})")
+                #axis[0].text(x[0], y[0], 'SP')
+                #axis[0].text(x[0], 300, stringout0)
+                #print(x, y)
+
+                axis[1].plot(_xin, _tcalcR, '-b', label='R-Ma-NRTL')
+                axis[1].plot(_xin, _tcalcRSR, '--b', label='Rac-Ma-Porter_GD')
+                axis[1].set_title("R-Rac-Porter_GD")
+                axis[1].set_ylabel('Temperatur [K]')
+                axis[1].set_xlabel('x-R-Ma [-]')
+                axis[1].legend(loc='center left')
+
+                line_3 = LineString(h.np.column_stack((_xin, _tcalcR)))
+                line_4 = LineString(h.np.column_stack((1-_xinRac, _tcalcRSR)))
+                intersection = line_3.intersection(line_4)
+
+                #axis[1].plot(*intersection.xy, 'ro')
+                #x2, y2 = intersection.xy
+                #xout1 = x2[0]
+                #xoutstr1 = "{:10.4f}".format(xout1)
+                #yout1 = y2[0]
+                #youtstr1 = "{:10.4f}".format(yout1)
+                #stringout1 = str(f"SP({xoutstr1}"f"\\{youtstr1})")
+                #axis[1].text(x2[0], y2[0], 'SP')
+                #axis[1].text(x2[0], 350, stringout1)
+                #print(x2, y2)
+
+                axis[2].plot(1-_xin[0:100], _tcalcS[0:100], '-g', label='S-Ma-NRTL')
+                axis[2].plot(_xin[0:100], _tcalcR[0:100], '-b', label='S-Ma-NRTL')
+                axis[2].plot(_xin, _tcalcRSS, '--g', label='Rac-Ma-Porter_GD')
+                axis[2].plot(1-_xin, _tcalcRSR, '--b', label='Rac-Ma-Porter_GD')
+                axis[2].plot(XEXP, TEXP, 'rx', label='Experimentaldaten')
+                axis[2].set_title("Konstruktion_Phasendiagramm")
+                axis[2].set_ylabel('Temperatur [K]')
+                axis[2].set_xlabel('x-R-Ma [-]')
+                axis[2].legend(loc='center left', fontsize=6)
+
+                plt.show()
+                _xinDF = h.pd.DataFrame(_xin, columns=['xin'])
+                _tSDF = h.pd.DataFrame(_tcalcS, columns=['t S'])
+                _tRSDF = h.pd.DataFrame(_tcalcRSS, columns=['t RS'])
+                _tSRDF = h.pd.DataFrame(_tcalcRSS, columns=['t SR'])
+                _tRDF = h.pd.DataFrame(_tcalcR, columns=['t R'])
+                _dataout = [_xinDF, _tSDF, _tRSDF, _tSRDF, _tRDF]
+                df = h.pd.concat(_dataout, axis=1)
+                df.to_excel(r'C:\\Users\\Ulf\\Desktop\\originData_beidseitig.xlsx')
+
+                return 0
+
             def _nrtl_nrtl():
                 _xin = h.np.zeros(100)
                 _xin[0] = 0
@@ -1123,7 +1232,8 @@ class EutFind:
                 #_nrtl_pure_comp_porter()
                 #_nrtl_pure_comp_porter_eigen()
                 #_nrtl_pure_comp_porter_eigen_GD()
-                _nrtl_pure_comp_SLE_eigen()
+                #_nrtl_pure_comp_SLE_eigen()
+                _nrtl_pure_comp_SLE_eigen_beidseitig()
                 #_nrtl_nrtl()
                 #_eut_test()
 
