127a8a85032ba1c74b5ca709e286604741714244
C34l <u.garbe95@gmail.com>
2022-10-05 08:36:43 +0200

    05.10.22 Gleichungen einfügen und sortieren, verfahrensplanung

--- Diagrammdaten.py
+++ Diagrammdaten.py
@@ -7,16 +7,35 @@ _r = 8.3141
 _t0S = 404.75
 _t0RS = 393.35
 _t0R = 404.65
+
 _h0S = 24500.0
-_h0S_fit = 28100.0
 _h0RS = 25600.0
-_h0RS_fit = 31200.0
 _h0R = 25736.0
-#_h0R_fit = 29936.0
-_h0R_fit = 28100.0
+
 _Alpha = 0.4
 XEXP = h.np.array([0.0, 0.1994, 0.3192, 0.4, 0.5, 0.5514, 0.605, 0.631, 0.6807, 0.6902, 0.7508, 0.7997, 0.8504, 0.9002, 0.9406, 1])
 TEXP = h.np.array([404.75, 393.65, 387.55, 391.35, 393.35, 392.85, 391.35, 390.95, 388.35, 387.95, 391.75, 393.65, 397.15, 399.15, 400.65, 404.65])
+
+_a1a = -5.597
+_a2a = 1952.672
+_aSa = h.np.array([_a1a, _a2a, ])
+
+_a1b = -32.196
+_a2b = 12794.746
+_aRa = h.np.array([_a1b, _a2b, ])
+
+_gabS = -1403.640
+_gbaS = 2972.166
+_gSa = h.np.array([_gabS, _gbaS, ])
+
+_gabRS = -3143.16
+_gbaRS = 9443.10
+_gRSa = h.np.array([_gabRS, _gbaRS, ])
+
+_gabR = -23.289
+_gbaR = 7526.385
+_gRa = h.np.array([_gabR, _gbaR, ])
+
 
 class Diagrams:
     @staticmethod
@@ -49,42 +68,82 @@ class Diagrams:
         _func = (h.np.exp((-_h0 / (_r * _texp)) * (1 - (_texp / _t0))) / _gx) - 1
         return _func
 
+    #ana
     @staticmethod
-    def PD_ideal():
+    def PD_ideal(x, t):
+        _func = h.np.log(1/((4*x)-4*x**2))-((_h0RS/_r)*((1/t)-(1/_t0RS)))
+        return _func
+
+    #num
+    @staticmethod
+    def PD_real_literatur(c, x, t):
+        _func = -(t/_h0RS)*((x/(1-x))-1)*(((_r*t)/x)-2*c*(1-x))
+        return _func
+
+    #num
+    @staticmethod
+    def PD_Porter_pia(a, x, t):
+        _func = ((_r*t**2)/_h0RS)*((1/(1-x))-(1/x)+2*a[0]*(-2*x+1)+2*a[1]*(((-2*x+1))/t))
+        return _func
+
+    #num
+    @staticmethod
+    def PD_NRTL_pia(g, x, t):
+        _func =
         return 0
 
     @staticmethod
-    def PD_real_literatur():
+    def PD_Porter_fit():
         return 0
 
     @staticmethod
-    def PD_Porter():
+    def PD_NRTL_fit():
         return 0
 
     @staticmethod
-    def PD_NRTL():
+    def Bilanz_A_porter_pia():
         return 0
 
     @staticmethod
-    def Bilanz_A_porter():
+    def Bilanz_A_nrtl_pia():
         return 0
 
     @staticmethod
-    def Bilanz_A_nrtl():
+    def Bilanz_A_porter_fit():
         return 0
 
     @staticmethod
-    def Bilanz_B_porter():
+    def Bilanz_A_nrtl_fit():
         return 0
 
     @staticmethod
-    def Bilanz_B_nrtl():
+    def Bilanz_B_porter_pia():
         return 0
 
     @staticmethod
-    def Bilanz_C_porter():
+    def Bilanz_B_nrtl_pia():
         return 0
 
     @staticmethod
-    def Bilanz_C_nrtl():
+    def Bilanz_B_porter_fit():
+        return 0
+
+    @staticmethod
+    def Bilanz_B_nrtl_fit():
+        return 0
+
+    @staticmethod
+    def Bilanz_C_porter_pia():
+        return 0
+
+    @staticmethod
+    def Bilanz_C_nrtl_pia():
+        return 0
+
+    @staticmethod
+    def Bilanz_C_porter_fit():
+        return 0
+
+    @staticmethod
+    def Bilanz_C_nrtl_fit():
         return 0
