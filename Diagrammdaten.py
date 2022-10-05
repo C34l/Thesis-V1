7c52992a448835bed26485a24f23526ce12b8283
C34l <u.garbe95@gmail.com>
2022-10-05 09:13:22 +0200

    05.10.22 Gleichungen einfügen und sortieren, verfahrensplanung

--- Diagrammdaten.py
+++ Diagrammdaten.py
@@ -14,7 +14,12 @@ _h0R = 25736.0
 
 _Alpha = 0.4
 XEXP = h.np.array([0.0, 0.1994, 0.3192, 0.4, 0.5, 0.5514, 0.605, 0.631, 0.6807, 0.6902, 0.7508, 0.7997, 0.8504, 0.9002, 0.9406, 1])
+XEXP_korr = h.np.array([0.0, 0.1994, 0.3192, 0.4, 0.5, 0.486, 0.395, 0.369, 0.3193, 0.3098, 0.2492, 0.2003, 0.1496, 0.0998, 0.0594, 0])
+XEXP_korr_links = h.np.array([0.0, 0.1994, 0.3192, 0.4, 0.5, ])
+XEXP_korr_rechts = h.np.array([0.5, 0.486, 0.395, 0.369, 0.3193, 0.3098, 0.2492, 0.2003, 0.1496, 0.0998, 0.0594, 0])
 TEXP = h.np.array([404.75, 393.65, 387.55, 391.35, 393.35, 392.85, 391.35, 390.95, 388.35, 387.95, 391.75, 393.65, 397.15, 399.15, 400.65, 404.65])
+TEXP_links = h.np.array([404.75, 393.65, 387.55, 391.35, 393.35, ])
+TEXP_rechts = h.np.array([393.35, 392.85, 391.35, 390.95, 388.35, 387.95, 391.75, 393.65, 397.15, 399.15, 400.65, 404.65])
 
 _a1a = -5.597
 _a2a = 1952.672
@@ -70,7 +75,7 @@ class Diagrams:
 
     #ana
     @staticmethod
-    def PD_ideal(x, t):
+    def PD_ideal(t, x):
         _func = h.np.log(1/((4*x)-4*x**2))-((_h0RS/_r)*((1/t)-(1/_t0RS)))
         return _func
 
@@ -86,22 +91,72 @@ class Diagrams:
         _func = ((_r*t**2)/_h0RS)*((1/(1-x))-(1/x)+2*a[0]*(-2*x+1)+2*a[1]*(((-2*x+1))/t))
         return _func
 
+    # num
+    @staticmethod
+    def PD_Porter_pia_num(x, t, a):
+        _tinitial = 393.35
+
+        _initial = [_tinitial]
+        _func = h.spi.solve_ivp(Diagrams.PD_Porter_pia, [0.5, 1.0], _initial, method='RK45',
+                                    args=(_Alpha, a[0], a[1], _h0RS), t_eval=x, dense_output=True)
+        return _func
+
     #num
     @staticmethod
     def PD_NRTL_pia(g, x, t):
-        _func =
-        return 0
+        a = _Alpha
+        u = g[0]
+        U = h.np.exp((a*u)/(_r*t))
+        v = g[1]
+        V = h.np.exp((a*v)/(_r*t))
+
+        _func = ((x/(1-x))-1)*(t/_h0RS)*(((_r*t)/x)+(2*(1-x)**2)*(((u*(U-1))/(x*(U-1)+1)**3)-((v*V*(V-1))/((x-1)*V-x)**3))-(2*(1-x))*((u/(x*(U-1)+1)**2)+((v*V)/(x-(x-1)*V)**2)))
+        return _func
+
+    #num
+    @staticmethod
+    def PD_NRTL_pia_num(x, t, g):
+        _tinitial = 393.35
+
+        _initial = [_tinitial]
+        _func = h.spi.solve_ivp(Diagrams.PD_NRTL_pia, [0.5, 1.0], _initial, method='RK45',
+                                args=(_Alpha, g[0], g[1], _h0RS), t_eval=x, dense_output=True)
+        return _func
 
     @staticmethod
-    def PD_Porter_fit():
-        return 0
+    def PD_Porter_fit_minfqs(_a, _xi, _texp, _h0, _t0, _steps,):
+        _tcalc = h.np.zeros(_steps)
+        _tdiff = h.np.zeros(_steps)
+
+        for x in range(_steps):
+            _tcalc1 = h.spo.fsolve(Diagrams.PD_Porter_pia_num, _texp[x], args=(_xi[x], _h0, _t0, _a,), full_output=True)
+            _tcalc[x] = _tcalc1[0]
+            _tdiff[x] = _texp[x] - _tcalc[x]
+        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
+
+        fqs_summe = h.np.sum(fqs_norm)
+
+        return fqs_summe
 
     @staticmethod
-    def PD_NRTL_fit():
-        return 0
+    def PD_NRTL_fit_minfqs(_g, _xi, _texp, _h0, _t0, _steps,):
+        _tcalc = h.np.zeros(_steps)
+        _tdiff = h.np.zeros(_steps)
 
+        for x in range(_steps):
+            _tcalc1 = h.spo.fsolve(Diagrams.PD_NRTL_pia_num, _texp[x], args=(_xi[x], _h0, _t0, _g,), full_output=True)
+            _tcalc[x] = _tcalc1[0]
+            _tdiff[x] = _texp[x] - _tcalc[x]
+        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
+
+        fqs_summe = h.np.sum(fqs_norm)
+
+        return fqs_summe
+
+    #ana
     @staticmethod
-    def Bilanz_A_porter_pia():
+    def Bilanz_A_porter_pia(a,x,t):
+        _func = (((1/2)*((-_h0S/(_r*t))+()))/())-1
         return 0
 
     @staticmethod
