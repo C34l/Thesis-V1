a42f275e733b5db31720595284a23a234766a5e1
C34l <u.garbe95@gmail.com>
2022-10-05 10:31:53 +0200

    05.10.22 Gleichungen überarbeiten, grundstruktur anlegen

--- Diagrammdaten.py
+++ Diagrammdaten.py
@@ -71,12 +71,12 @@ class Diagrams:
 
     @staticmethod
     def t_sle_porter(_texp, _xi, _h0, _t0, _g, _alpha):
-        _yi = Diagrams.y_porter(_g, _xi, _texp, _alpha)
+        _yi = Diagrams.y_porter(_g, _xi, _texp,)
         _gx = h.np.exp(_yi) * _xi
         _func = (h.np.exp((-_h0 / (_r * _texp)) * (1 - (_texp / _t0))) / _gx) - 1
         return _func
 
-    #ana
+    #ana, löst PD ideal
     @staticmethod
     def PD_ideal(t, x):
         _func = h.np.log(1/((4*x)-4*x**2))-((_h0RS/_r)*((1/t)-(1/_t0RS)))
@@ -88,25 +88,40 @@ class Diagrams:
         _func = -(t/_h0RS)*((x/(1-x))-1)*(((_r*t)/x)-2*c*(1-x))
         return _func
 
-    #num
+    #num, Gleichung PD Porter Pia
     @staticmethod
-    def PD_Porter_pia(a, x, t):
+    def PD_Porter_pia(x, t, a,):
         _func = ((_r*t**2)/_h0RS)*((1/(1-x))-(1/x)+2*a[0]*(-2*x+1)+2*a[1]*(((-2*x+1))/t))
         return _func
 
-    # num
+    # num, löst PD porter Pia
     @staticmethod
-    def PD_Porter_pia_num(x, t, a):
+    def PD_Porter_pia_num(t, x, a):
         _tinitial = 393.35
 
         _initial = [_tinitial]
-        _func = h.spi.solve_ivp(Diagrams.PD_Porter_pia, [0.5, 1.0], _initial, method='RK45',
-                                    args=(_Alpha, a[0], a[1], _h0RS), t_eval=x, dense_output=True)
+        _func = h.spi.solve_ivp(Diagrams.PD_Porter_pia, [0.5, 1.0], _initial, method='RK45', args=(a, ), t_eval=x, dense_output=True)
         return _func
 
-    #num
+    # soll PD-Porter optimieren und a bestimmen
     @staticmethod
-    def PD_NRTL_pia(g, x, t):
+    def PD_Porter_fit_minfqs(_a, _xi, _texp, _h0, _t0, _steps, ):
+        _tcalc = h.np.zeros(_steps)
+        _tdiff = h.np.zeros(_steps)
+
+        for x in range(_steps):
+            _tcalc1 = h.spo.fsolve(Diagrams.PD_Porter_pia_num, _texp[x], args=(_xi, _a,), full_output=True)
+            _tcalc[x] = _tcalc1[0]
+            _tdiff[x] = _texp[x] - _tcalc[x]
+        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
+
+        fqs_summe = h.np.sum(fqs_norm)
+
+        return fqs_summe
+
+    #num, Gleichung PD-Nrtl-Pia
+    @staticmethod
+    def PD_NRTL_pia(x, t, g,):
         a = _Alpha
         u = g[0]
         U = h.np.exp((a*u)/(_r*t))
@@ -116,31 +131,17 @@ class Diagrams:
         _func = ((x/(1-x))-1)*(t/_h0RS)*(((_r*t)/x)+(2*(1-x)**2)*(((u*(U-1))/(x*(U-1)+1)**3)-((v*V*(V-1))/((x-1)*V-x)**3))-(2*(1-x))*((u/(x*(U-1)+1)**2)+((v*V)/(x-(x-1)*V)**2)))
         return _func
 
-    #num
+    #num, löst PD-Nrtl-Pia
     @staticmethod
-    def PD_NRTL_pia_num(x, t, g):
+    def PD_NRTL_pia_num(t, x, g):
         _tinitial = 393.35
 
         _initial = [_tinitial]
         _func = h.spi.solve_ivp(Diagrams.PD_NRTL_pia, [0.5, 1.0], _initial, method='RK45',
-                                args=(_Alpha, g[0], g[1], _h0RS), t_eval=x, dense_output=True)
+                                args=(g), t_eval=x, dense_output=True)
         return _func
 
-    @staticmethod
-    def PD_Porter_fit_minfqs(_a, _xi, _texp, _h0, _t0, _steps,):
-        _tcalc = h.np.zeros(_steps)
-        _tdiff = h.np.zeros(_steps)
-
-        for x in range(_steps):
-            _tcalc1 = h.spo.fsolve(Diagrams.PD_Porter_pia_num, _texp[x], args=(_xi[x], _h0, _t0, _a,), full_output=True)
-            _tcalc[x] = _tcalc1[0]
-            _tdiff[x] = _texp[x] - _tcalc[x]
-        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
-
-        fqs_summe = h.np.sum(fqs_norm)
-
-        return fqs_summe
-
+    #num, soll PD-NRTL optimieren und g bestimmen
     @staticmethod
     def PD_NRTL_fit_minfqs(_g, _xi, _texp, _h0, _t0, _steps,):
         _tcalc = h.np.zeros(_steps)
@@ -156,46 +157,24 @@ class Diagrams:
 
         return fqs_summe
 
-    #ana
+    #ana, lässt t berechnen
     @staticmethod
     def Bilanz_A_porter_pia(t, x, a):
-        ya = Diagrams.y_porter(x, t, a[0])
-        yb = Diagrams.y_porter(x, t, a[1])
+        A = h.np.array([a[0], a[1]])
+        B = h.np.array([a[2], a[3]])
+        ya = Diagrams.y_porter(x, t, A)
+        yb = Diagrams.y_porter(x, t, B)
         _func = (((1/2)*(((-_h0S/(_r*t))*(1-(t/_t0S)))+(((-_h0R/(_r*t))*(1-(t/_t0R))))))/((h.np.log(0.25)-h.np.log(x*ya*(1-x)*yb))))-1
         return _func
 
-    #ana
+    #soll A Porter optimieren und as bestimmen
     @staticmethod
-    def Bilanz_A_nrtl_pia(t, x, g):
-        ya = Diagrams.y_nrtl(g[0], x, t, _Alpha)
-        yb = Diagrams.y_nrtl(g[1], x, t, _Alpha)
-        _func = (((1 / 2) * (((-_h0S / (_r * t)) * (1 - (t / _t0S))) + (((-_h0R / (_r * t)) * (1 - (t / _t0R)))))) / (
-         (h.np.log(0.25) - h.np.log(x * ya * (1 - x) * yb)))) - 1
-        return _func
-
-    @staticmethod
-    def Bilanz_A_porter_fit_minfqs(_a, _xi, _texp, _h0, _t0, _steps,):
+    def Bilanz_A_porter_fit_minfqs(_a, _xi, _texp, _steps, ):
         _tcalc = h.np.zeros(_steps)
         _tdiff = h.np.zeros(_steps)
 
         for x in range(_steps):
-            _tcalc1 = h.spo.fsolve(Diagrams.Bilanz_A_porter_pia, _texp[x], args=(_xi[x], _h0, _t0, _a,), full_output=True)
-            _tcalc[x] = _tcalc1[0]
-            _tdiff[x] = _texp[x] - _tcalc[x]
-        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
-
-        fqs_summe = h.np.sum(fqs_norm)
-
-        return fqs_summe
-
-    @staticmethod
-    def Bilanz_A_nrtl_fit_minfqs(_g, _xi, _texp, _h0, _t0, _steps,):
-        _tcalc = h.np.zeros(_steps)
-        _tdiff = h.np.zeros(_steps)
-
-
-        for x in range(_steps):
-            _tcalc1 = h.spo.fsolve(Diagrams.Bilanz_A_nrtl_pia, _texp[x], args=(_xi[x], _h0, _t0, _g,),
+            _tcalc1 = h.spo.fsolve(Diagrams.Bilanz_A_porter_pia, _texp[x], args=(_xi[x], _a,),
                                    full_output=True)
             _tcalc[x] = _tcalc1[0]
             _tdiff[x] = _texp[x] - _tcalc[x]
@@ -205,26 +184,63 @@ class Diagrams:
 
         return fqs_summe
 
+    #ana, lässt t berechnen
     @staticmethod
-    def Bilanz_B_porter_pia(t, x):
-        ya = Diagrams.y_porter(x, t, _aSa[0], _aSa[1])
-        yb = Diagrams.y_porter(x, t, _aRa[0], _aRa[1])
+    def Bilanz_A_nrtl_pia(t, x, g):
+        A = h.np.array([g[0], g[1]])
+        B = h.np.array([g[2], g[3]])
+        ya = Diagrams.y_nrtl(A, x, t, _Alpha)
+        yb = Diagrams.y_nrtl(B, x, t, _Alpha)
+        _func = (((1 / 2) * (((-_h0S / (_r * t)) * (1 - (t / _t0S))) + (((-_h0R / (_r * t)) * (1 - (t / _t0R)))))) / (
+         (h.np.log(0.25) - h.np.log(x * ya * (1 - x) * yb)))) - 1
+        return _func
+
+    #soll nrtl koeffizienten bestimmen
+    @staticmethod
+    def Bilanz_A_nrtl_fit_minfqs(_g, _xi, _texp, _steps,):
+        _tcalc = h.np.zeros(_steps)
+        _tdiff = h.np.zeros(_steps)
+
+        for x in range(_steps):
+            _tcalc1 = h.spo.fsolve(Diagrams.Bilanz_A_nrtl_pia, _texp[x], args=(_xi[x], _g,),
+                                   full_output=True)
+            _tcalc[x] = _tcalc1[0]
+            _tdiff[x] = _texp[x] - _tcalc[x]
+        fqs_norm = (h.np.abs(h.np.divide(_tdiff, _texp))) ** 2
+
+        fqs_summe = h.np.sum(fqs_norm)
+
+        return fqs_summe
+
+    #berechnet t für Ansatz B
+    @staticmethod
+    def Bilanz_B_porter_pia(t, x, a):
+        A = h.np.array([a[0], a[1]])
+        B = h.np.array([a[2], a[3]])
+        ya = Diagrams.y_porter(x, t, A)
+        yb = Diagrams.y_porter(x, t, B)
+
         _func = (((1 / 2) * (((-_h0S / (_r * t)) * (1 - (t / _t0S))) + (((-_h0R / (_r * t)) * (1 - (t / _t0R)))))) / (
          (h.np.log(4 * x * ya * (1 - x) * yb)))) - 1
         return _func
 
-    @staticmethod
-    def Bilanz_B_nrtl_pia(t, x):
-        ya = Diagrams.y_nrtl(_gSa, x, t, _Alpha)
-        yb = Diagrams.y_nrtl(_gRa, x, t, _Alpha)
-        _func = (((1 / 2) * (((-_h0S / (_r * t)) * (1 - (t / _t0S))) + (((-_h0R / (_r * t)) * (1 - (t / _t0R)))))) / (
-            (h.np.log(4 * x * ya * (1 - x) * yb)))) - 1
-        return _func
-
+    #soll Porterkoeff für Ansatz B optimieren
     @staticmethod
     def Bilanz_B_porter_fit_minfqs():
         return 0
 
+    #soll t für Ansatz B nrtl berechnen
+    @staticmethod
+    def Bilanz_B_nrtl_pia(t, x, g):
+        A = h.np.array([g[0], g[1]])
+        B = h.np.array([g[2], g[3]])
+        ya = Diagrams.y_nrtl(A, x, t, _Alpha)
+        yb = Diagrams.y_nrtl(B, x, t, _Alpha)
+        _func = (((1 / 2) * (((-_h0S / (_r * t)) * (1 - (t / _t0S))) + (((-_h0R / (_r * t)) * (1 - (t / _t0R)))))) / (
+            (h.np.log(4 * x * ya * (1 - x) * yb)))) - 1
+        return _func
+
+    #soll für ansatz B g's optimieren
     @staticmethod
     def Bilanz_B_nrtl_fit_minfqs(g1, g2, _xi, _texp, _h0, _t0, _steps,):
         _tcalc = h.np.zeros(_steps)
