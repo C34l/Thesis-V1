882ecbb6c69f2d7b1dcd74f1af27fd23636821c3
C34l <u.garbe95@gmail.com>
2023-07-15 15:17:39 +0200

    1507 begin of equations

--- Promo_Model_1.py
+++ Promo_Model_1.py
@@ -10,34 +10,35 @@ class fit_functions_binary:
     #calculating activity coefficient for margules
     @staticmethod
     def y_margules(_t, _x, _a):
-        return 0;
+        return 0
 
     # calculating activity coefficient for porter with 3 coefficients
     @staticmethod
     def y_porter(_t, _x, _a1, _a2, _a3):
-        return 0;
+        _func = h.np.exp((_a1+_a2/_t+_a3/_t**2)*(1-_x)**2)
+        return _func
 
     # calculating activity coefficient for koningsveld? or whole different model?
     @staticmethod
     def y_koningsveld(_t, _x, _a1, _a2, _a3, _p):
-        return 0;
+        return 0
 
     # calculating activity coefficient for nrtl
     @staticmethod
     def y_nrtl():
-        return 0;
+        return 0
 
     # basic function for every possible gamma, maybe need for optimized forms depending on model
     @staticmethod
     def eq_for_x():
-        return 0;
+        return 0
 
     # equation to solve EQs for T, again model dependent
     @staticmethod
     def eq_for_T():
-        return 0;
+        return 0
 
     #criteria for optimization of system, quality criterium
     @staticmethod
     def min_fqs():
-        return 0;
+        return 0
