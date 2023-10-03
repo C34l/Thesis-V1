2e135d52c1d7da620a3b8180fb605f10574c3da5
C34l <u.garbe95@gmail.com>
2023-10-03 15:02:06 +0200

    03102023 koningsveld implementation

--- Koningsveld_model.py
+++ Koningsveld_model.py
@@ -21,7 +21,7 @@ def objective_function(_xAexp, _xBexp, _steps, _texp):
     return OF
 
 
-def phase_balance(x_1_alpha, x_1_beta, p):
+def phase_balance(p, x_1_alpha, x_1_beta, _texp):
     _x_2_alpha = 1 - x_1_alpha
     _x_2_beta = 1 - x_1_beta
 
@@ -30,8 +30,37 @@ def phase_balance(x_1_alpha, x_1_beta, p):
     con3 = ((1-x_1_alpha)**2)/((1-x_1_alpha*p)**2)
     eq1 = con1/(con2-con3)
 
-    con4 =
-    con5 =
-    con6 =
+    con4 = (h.np.log(1-x_1_alpha/1-x_1_beta))
+    con5 = x_1_beta**2 * (1-p) / (1-x_1_beta*p)**2
+    con6 = x_1_alpha**2 * (1-p) / (1-x_1_alpha*p)**2
+    eq2 = con4/(con5-con6)
 
-    return 0
+    a= 0.9499 + 12.587/_texp
+
+    #_func = 1 - (eq2/eq1)
+    _func = eq1 - a
+
+    return _func
+
+def x_crit(x,p):
+    a = 2*x-1/(x*(x-2))
+    func = 1 - a / p
+    return func
+
+
+guess_p = 0.9
+guess_x = 0.2
+t_exp_01593 = h.np.array([273.15, 282.65, 292.75, 302.65, 312.45, 322.35, 332.35])
+x_1_alpha = h.np.array([0.00155311, 0.00141498, 0.00126175, 0.00120053, 0.00112407, 0.00116994, 0.00120053])
+x_1_beta = h.np.array([0.997587, 0.996519, 0.995637, 0.994455, 0.992705, 0.991104, 0.989026])
+steps = len(x_1_alpha)
+
+
+
+p_output = h.spo.least_squares(phase_balance, guess_p, args=(x_1_alpha,x_1_beta,t_exp_01593),bounds=((-20,1)))
+x_c = h.spo.fsolve(x_crit, guess_x, args=(p_output.x))
+print('value for p:', p_output.x)
+print('value for xc:', x_c)
+
+
+
